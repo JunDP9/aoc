0c97f576b5b16f52caee860a3ef7035c0a3acd75
jchatani <jun.chatani@persgroep.net>
2021-12-06 11:39:50 +0100

    quick and dirty

--- day6/main.py
+++ day6/main.py
@@ -1,38 +1,44 @@
-import numpy as np
-
-
 def input_file(filename):
     coefficients = []
     with open(filename, 'rt') as file:
         for line in file:  # loop over each line
-            coefficients= (str(line)
-                                .replace(',', ' ')
-                                )  # parse them in some way
+            coefficients = (str(line)
+                            .replace(',', ' ')
+                            )  # parse them in some way
     return coefficients
 
-def grow(mylist, amount_days):
-    for idx in range(0, amount_days):
-        mylist = [item-1 for item in mylist] #subtract 1
-        mylist = mylist+([8]*mylist.count(-1))
-        mylist = [6 if item == -1 else item for item in mylist]
 
-    print(len(mylist))
+def grow(mydict, mylist, amount_days):
+    for idx in range(0, amount_days):
+        for key, value in mydict.items():
+            if key is not -1:
+                mydict[(int(key) - 1)] = value
+
+        mydict[8] = mydict[(-1)]
+        mydict[6] = mydict[6] + mydict[(-1)]
+        mydict[(-1)] = 0
+
     return mylist
-        # subtract 1
-        # count amount -1
-        # add 8 for the counted amount
-        # replace all occurences of -1 by 6
 
 
 def main():
-    line = input_file('input2.txt')
+    line = input_file('input.txt')
     mylist = [int(x) for x in line.split(' ')]
-    # test = [0,4,6,3]
-    # print(test)
-    print(len(mylist))
+    mydict = {}
+    mydict[(-1)] = mylist.count(-1)
+    mydict[0] = mylist.count(0)
+    mydict[1] = mylist.count(1)
+    mydict[2] = mylist.count(2)
+    mydict[3] = mylist.count(3)
+    mydict[4] = mylist.count(4)
+    mydict[5] = mylist.count(5)
+    mydict[6] = mylist.count(6)
+    mydict[7] = mylist.count(7)
+    mydict[8] = mylist.count(8)
 
-    grow(mylist, 256)
-
+    print(sum(mydict.values()))
+    grow(mydict, mylist, 256)
+    print(sum(mydict.values()))
 
 
 main()
