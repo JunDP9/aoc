4a21dcdef3fb8e2e31b56d6b1b3e3fe979e5c199
jchatani <jun.chatani@persgroep.net>
2021-12-15 19:16:03 +0100

    some bugs left

--- day14/main.py
+++ day14/main.py
@@ -1,3 +1,4 @@
+import math
 from collections import Counter
 from operator import itemgetter
 import time
@@ -44,37 +45,84 @@ def compute_v1(my_dict, starting_string, steps):
     return max_count, max_key, min_count, min_key
 
 
-def compute_v2(my_dict, starting_string, steps):
+def compute_v2(inter_total_dict, total_dict, comp_dict, steps):
     start_time = time.time()
+    # print(comp_dict)
     for step in range(0, steps):
-        print(step)
-        padded_list = ['XXX']*(2*len(starting_string)-1)
-        padded_list[::2] = starting_string
-        for idx in range(1, len(padded_list), 2):
-            char_combo = (padded_list[idx-1] + padded_list[idx+1])
-            if char_combo in my_dict:
-                padded_list[idx] = my_dict[char_combo]
-        starting_string = padded_list
+        empty_total_dict = inter_total_dict.copy()
+        for key,value in total_dict.items():
+            empty_total_dict[comp_dict[key][0]] += value
+            empty_total_dict[comp_dict[key][1]] += value
+            # inter_total_dict[key] = 0
+        total_dict = (empty_total_dict)
 
-    counter = Counter(starting_string)
+    # print(total_dict)
+    final_dict = make_char_dict(total_dict)
+    # print(final_dict)
+
+    counter = Counter(final_dict)
     min_key, min_count = min(counter.items(), key=itemgetter(1))
     max_key, max_count = max(counter.items(), key=itemgetter(1))
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    return max_count, max_key, min_count, min_key
+    return max_count/2, max_key, min_count/2, min_key
+
+
+def make_char_dict(total_dict):
+    final_dict = {}
+    for key, value in total_dict.items():
+        split_char_one = list(key)[0]
+        split_char_two = list(key)[1]
+        if split_char_one in final_dict:
+            final_dict[split_char_one] += value
+        if split_char_two in final_dict:
+            final_dict[split_char_two] += value
+        if split_char_one not in final_dict:
+            final_dict[split_char_one] = value
+        if split_char_two not in final_dict:
+            final_dict[split_char_two] = value
+    return final_dict
+
+
+def make_comp(my_dict):
+    comp_dict={}
+    total_dict={}
+    for key, value in my_dict.items():
+        char_combo = list(key)
+        char_combo_child_one = char_combo[0] + value
+        char_combo_child_two = value + char_combo[1]
+        comp_dict[key] = (char_combo_child_one, char_combo_child_two)
+        if key not in total_dict:
+            total_dict[key] = 0
+        if char_combo_child_one not in total_dict:
+            total_dict[char_combo_child_one] = 0
+        if char_combo_child_two not in total_dict:
+            total_dict[char_combo_child_two] = 0
+    return [comp_dict, total_dict]
 
 
 def part_two(inputs):
     starting_string = list(inputs[0])
     my_dict = inputs[1]
-    max_count, max_key, min_count, min_key = compute_v2(my_dict, starting_string, 40)
+    extra_dicts = make_comp(my_dict)
+    comp_dict = extra_dicts[0]
+    total_dict = extra_dicts[1]
+
+    inter_total_dict = total_dict.copy()
+    for idx in range(1, len(starting_string)):
+        char_combo = (starting_string[idx - 1] + starting_string[idx])
+        if char_combo in total_dict:
+            total_dict[char_combo] = 1
+    char_dict = make_char_dict(total_dict)
+    # print(char_dict)
+    max_count, max_key, min_count, min_key =  compute_v2(inter_total_dict, total_dict, comp_dict, 40)
     print(min_key, min_count)
     print(max_key, max_count)
-    print(max_count - min_count)
+    print(math.ceil((max_count - min_count)))
 
 
 def main():
-    inputs = input_file('input.txt')
+    inputs = input_file('input2.txt')
     # part_one(inputs)
     part_two(inputs)
 
